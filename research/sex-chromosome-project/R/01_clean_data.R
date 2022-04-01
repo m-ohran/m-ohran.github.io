ab170dcb041e862325ba728d4d9f3dc34dabd66f
Marissa <marissa.ohran@gmail.com>
2022-03-31 20:27:16 -0600

    testing a thing

--- research/sex-chromosome-project/R/01_clean_data.R
+++ research/sex-chromosome-project/R/01_clean_data.R
@@ -4,13 +4,15 @@ library(readxl)
 library(janitor)
 
 # read data, remove header row
-dat <- read_xlsx("./Data/Raw/Sex_chromosome_genome_size_data.xlsx", )
+dat <- read_xlsx("./research/sex-chromosome-project/Data/Raw/Sex_chromosome_genome_size_data.xlsx", range = "A2:X416")
 
 # clean names
 dat <- dat %>% clean_names()
 
-# remove columns I don't want
-dat <- dat %>% select(-c(gs_fem_pg,gs_male_pg,gs_sex_unknown_pg,gs_data_y_n,method,standard_species,std_sp_gs_pg,gs_source,source))
-
-# write clean spreadsheet
-write_
+# filter by whether or not we have gs data, and by what species have lifespan data for both sexes
+ dat <- dat %>% 
+  filter(gs_data_y_n == "Y", ! is.na(homogametic_lifespan), ! is.na(heterogametic_lifespan))
+ 
+# eliminate columns I don't want/need?
+ dat %>% select(-c(gs_sex_unknown_pg, gs_data_y_n)) %>% glimpse
+ 
