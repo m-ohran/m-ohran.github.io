b17c97ec40b9f8a789b939ffd1d2796098091f96
Marissa <marissa.ohran@gmail.com>
2022-05-03 02:36:51 -0600

    Updates

--- research/sex-chromosome-project/R/01_clean_data.R
+++ research/sex-chromosome-project/R/01_clean_data.R
@@ -4,7 +4,7 @@ library(readxl)
 library(janitor)
 
 # read data, remove header row
-dat <- read_xlsx("./research/sex-chromosome-project/Data/Raw/Sex_chromosome_genome_size_data.xlsx", range = "D2:U416")
+dat <- read_xlsx("./research/sex-chromosome-project/Data/Raw/Sex_chromosome_genome_size_data.xlsx", range = "A2:U416")
 
 # clean names
 dat <- dat %>% clean_names()
@@ -13,13 +13,6 @@ dat <- dat %>% clean_names()
  dat <- dat %>% 
   filter(gs_data_y_n == "Y", ! is.na(homogametic_lifespan), ! is.na(heterogametic_lifespan))
  
- # remove the weird cockroach?, we're not sure the GS values are correct
- # also, remove one of the melanogaster rows? combine them? remove them both?
- # what's up with the sheep (Ovis aries) GS?
-
-
- # lifespan is split by heterogametic and homogametic, GS is split male/female, should they be organized the same way for comparison?
- 
  # create homo/hetero GS columns
  
  dat <- dat %>%
@@ -27,14 +20,20 @@ dat <- dat %>% clean_names()
    mutate("gs_homo_mb" = if_else(sex_determination == "female heterogametic", gs_male_mb, gs_female_mb))
 
 
-
  # add GS diff by determination column
  
  dat <- dat %>% 
    mutate("gs_diff_det_mb" = gs_homo_mb*2 - gs_het_mb*2)
  
+ # add lifespan diff column
+ 
+ dat <-  dat %>% 
+   mutate("lifespan_diff" = homogametic_lifespan - heterogametic_lifespan)
+ 
  # remove columns I don't care about
  clean <- dat %>% select(-c(ends_with("_pg"), ends_with("_sample_size_n"), ends_with("_sd"), gs_data_y_n,gs_female_mb, gs_male_mb, gs_diff_mb))
+ 
+ # convert class, order, family, sex determination, population source, and data type to factors
  
  write_csv(clean, "./research/sex-chromosome-project/Data/Clean/clean_sex_chromosome_data.csv")
  
--- research/sex-chromosome-project/R/02_visualize_data.R
+++ research/sex-chromosome-project/R/02_visualize_data.R
@@ -4,7 +4,25 @@ library(tidyverse)
 # source clean data
 source("research/sex-chromosome-project/R/01_clean_data.R")
 
-# x= GS diff by determination, y = lifespan lrr # I think this might be the winner
+
+# x = GS diff by determination, y = lifespan diff
+clean %>% 
+  ggplot(aes(x = gs_diff_det_mb, y = lifespan_diff, color = sex_determination)) +
+  geom_point(size = 3) +
+  labs(x = "Genome Size Difference (mb)", y = "Lifespan Difference", color = "Sex Determination") +
+  theme_minimal() +
+  scale_color_manual(values = c("female heterogametic" = "darkmagenta",
+                                "male heterogametic" = "#3EB489")) +
+  theme(axis.title.x = element_text(face = "bold", size = 13),
+        axis.title.y = element_text(face = "bold", size = 13),
+        axis.text.x = element_text(face = "bold", size = 11),
+        axis.text.y = element_text(face = "bold", size = 11),
+        legend.text = element_text(face = "bold", size = 10),
+        legend.title = element_text(face = "bold", size = 12))
+
+ggsave("research/sex-chromosome-project/Output/Figs/GS_diff_Lifespan_diff.png", height = 10, width = 10, dpi = 300)
+
+# x= GS diff by determination, y = lifespan lrr
 clean %>% 
   ggplot(aes(x = gs_diff_det_mb, y = ln_r_rlifespan, color = sex_determination)) +
   geom_point(size = 3) +
@@ -22,12 +40,32 @@ clean %>%
 
 ggsave("research/sex-chromosome-project/Output/Figs/GS_diff_Lifespan_lrr.png", height = 10, width = 10, dpi = 300)
 
+# Drosophila plots?
 
-# Closer Look
+# x = GS diff by determination, y = lifespan diff
 clean %>% 
+  filter(family == "Drosophilidae") %>% 
+  ggplot(aes(x = gs_diff_det_mb, y = lifespan_diff, color = sex_determination)) +
+  geom_point(size = 3) +
+  labs(x = "Genome Size Difference (mb)", y = "Lifespan Difference", color = "Sex Determination") +
+  theme_minimal() +
+  scale_color_manual(values = c("female heterogametic" = "darkmagenta",
+                                "male heterogametic" = "#3EB489")) +
+  theme(axis.title.x = element_text(face = "bold", size = 13),
+        axis.title.y = element_text(face = "bold", size = 13),
+        axis.text.x = element_text(face = "bold", size = 11),
+        axis.text.y = element_text(face = "bold", size = 11),
+        legend.text = element_text(face = "bold", size = 10),
+        legend.title = element_text(face = "bold", size = 12))
+
+ggsave("research/sex-chromosome-project/Output/Figs/GS_diff_Lifespan_diff_dros.png", height = 10, width = 10, dpi = 300)
+
+
+# x= GS diff by determination, y = lifespan lrr
+clean %>% 
+  filter(family == "Drosophilidae") %>% 
   ggplot(aes(x = gs_diff_det_mb, y = ln_r_rlifespan, color = sex_determination)) +
   geom_point(size = 3) +
-  coord_cartesian(xlim = c(-250,250)) +
   labs(x = "Genome Size Difference (mb)", y = "Lifespan Log Response Ratio", color = "Sex Determination") +
   theme_minimal() +
   scale_color_manual(values = c("female heterogametic" = "darkmagenta",
@@ -39,5 +77,5 @@ clean %>%
         legend.text = element_text(face = "bold", size = 10),
         legend.title = element_text(face = "bold", size = 12))
 
-ggsave("research/sex-chromosome-project/Output/Figs/Zoomed_GS_diff_Lifespan_lrr.png", height = 10, width = 10, dpi = 300)
-                  
+
+ggsave("research/sex-chromosome-project/Output/Figs/GS_diff_Lifespan_lrr_dros.png", height = 10, width = 10, dpi = 300)
